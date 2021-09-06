98b5c9e284b6012562831d70ab3075ba0f064388
mironenko_ev@mail.ru <mironenko_ev@mail.ru>
2021-09-06 21:57:11 +0500

    Add project files.

---
+++ VSTAVKA/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+void printArray(const int array[], size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+		cout << array[i] << " ";
+}
+int* insertArray(int array[], size_t size, size_t index, int value)
+{
+	int* copy = new int[size + 1];
+	for (size_t i = 0; i < index; i++)
+		copy[i] = array[i];
+	copy[index] = value;
+	for (size_t i = index + 1; i <= size; i++)
+		copy[i] = array[i - 1];
+	return copy;
+}
+int main()
+{
+	size_t size;
+	cout << "Enter size of array: ";
+	cin >> size;
+	int* array = new int[size];
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << "Enter" << i << " element of array:";
+		cin >> array[i];
+	}
+	cout << "Your array:";
+	printArray(array, size);
+	size_t index;
+	size_t value;
+	cout << "Enter index of insertion to array:";
+	cin >> index;
+	cout << "Enter value to insert to array:";
+	cin >> value;
+	int* result = insertArray(array, size, index, value);
+	insertArray(array, size, index, value);
+	cout << "Your inserted array:";
+	printArray(result, size + 1);
+	delete[] result;
+	delete[] array;
+	
+	return 0;
+}
